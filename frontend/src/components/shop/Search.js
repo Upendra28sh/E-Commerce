ea61489cc58ece48941b658d02a787d2cb1ff0a3
Dhruv Ramdev <pythonbrilliant@gmail.com>
2018-07-31 18:01:48 +0530

    Refactored CSS with BEM and Changed Looks On Search Page

--- frontend/src/components/shop/Search.js
+++ frontend/src/components/shop/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col } from 'antd';
-import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
+import {Row, Col} from 'antd';
+import {connect} from 'react-redux';
 import * as actions from '../../actions/shop';
 
 class Search extends React.Component {
@@ -17,49 +17,47 @@ class Search extends React.Component {
                 if (product.description.toLowerCase().includes(query)) {
                     count += 1;
                 }
-        })
-        return <h2> {count} products found</h2> 
+            });
+        return <span><span>{count}</span> products found</span>;
     }
 
     render() {
         const query = this.props.match.params.query.trim().toLowerCase();
 
         return (
-            <div>
-                <Row>
-                    <Col span={10}>
-                        <img id="search" src="/7ad35c4c.png" />
-                    </Col>
-                    <Col span={14} id="search_result">
-                        <p>#{query}</p>
-                        { this.renderCount(query) }
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={24}>
-                        <div className="products">
-                            {
-                                this.props.products.map((product, index) => {
-                                    if (product.description.toLowerCase().includes(query)) {
-                                        return (
-                                            <Link 
-                                                key={index} 
-                                                to={"/shop/" + (index + 1)}
-                                            >
-                                                <img 
-                                                    className="each_product" 
-                                                    alt={product.description} 
-                                                    key={index} 
-                                                    src={product.product_url}
-                                                />
+            <div className=' bg-grey'>
+                <div className="container">
+                    <div className="">
+                        <Row className='search-result'>
+                            <Col span={6}>
+                                <div className='search-result__image'>
+                                    <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV9e0xcioHeH_3D7blQUumfnZQEgdveoWYdhEtP8qgGEN_xSxf"/>
+                                </div>
+                            </Col>
+                            <Col span={18} className="search-result__text">
+                                <h1>#{query}</h1>
+                                {this.renderCount(query)}
+                            </Col>
+                        </Row>
+                    </div>
+                    <Row>
+                        <Col span={24}>
+                            <div className="products">
+                                {this.props.products.map((image, index) =>
+                                    <div key={index} className={'product'}>
+                                        <div className='image-container'>
+                                            <Link to={"/shop/" + (index + 1)}>
+                                                <img className="img_fluid" // each_product
+                                                     alt={image.description} key={index}
+                                                     src={image.image_url}/>
                                             </Link>
-                                        );
-                                    }
-                                })
-                            }
-                        </div>
-                    </Col>
-                </Row>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         );
     }
