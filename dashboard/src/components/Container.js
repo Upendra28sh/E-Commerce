0befc564b3a0fefc98bf58ede97b11428e918c64
saurabhmittal16 <saurabhmittal1606@gmail.com>
2018-09-03 17:08:52 +0530

    Add seller dashboard app

---
+++ dashboard/src/components/Container.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Layout, Menu, Icon, Divider} from 'antd';
+import {Link} from 'react-router-dom';
+import Router from './Router/AppRouter';
+
+const {Header, Content, Footer, Sider} = Layout;
+
+class Container extends React.Component {
+    render() {
+        return (
+            <Layout className={'admin-panel'}>
+                <Sider style={{overflow: 'auto', height: '100vh', position: 'fixed', left: 0}}>
+                    <div className="logo"/>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Link to={'/'}>
+                                <Icon type="user"/>
+                                <span className="nav-text">Dashboard</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to={'/sellers'}>
+                                <Icon type="user"/>
+                                <span className="nav-text">Sellers</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Link to={'/orders'}>
+                                <Icon type="shopping-cart"/>
+                                <span className="nav-text">Orders</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="4">
+                            <Link to={'/users'}>
+                                <Icon type="user"/>
+                                <span className="nav-text">Users</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="5">
+                            <Link to={'/products'}>
+                                <Icon type="user"/>
+                                <span className="nav-text">Products</span>
+                            </Link>
+                        </Menu.Item>
+                        <Divider/>
+                    </Menu>
+                </Sider>
+                <Layout style={{marginLeft: 200}}>
+                    <Header style={{background: '#fff', padding: 0}}/>
+                    <Content style={{margin: '24px 16px 0', overflow: 'initial'}}>
+                        <div style={{padding: 24}}>
+                            <Router />
+                        </div>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>
+                        Ant Design ©2018 Created by Ant UED
+                    </Footer>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
+export default Container;
---
+++ dashboard/src/components/Query/query.js
@@ -0,0 +1,103 @@
+import { gql } from 'apollo-boost';
+
+// sellerID = "5b883a829a14eb330b094d8b"
+
+export const GET_ALL_USERS = gql`
+    query {
+        allUsers {
+            username
+            name
+            image
+            about
+            email
+            following {
+                id
+            }
+            followers {
+                id
+            }
+            followingShop {
+                id
+            }
+        }
+    }
+`;
+
+export const GET_ALL_SELLERS = gql `
+    query {
+        allSellers {
+            id
+            shopname
+            image
+            name
+            about
+        }
+    }
+`;
+
+export const GET_ALL_ORDERS = gql`
+    query {
+        getOrdersBySeller(sellerID: "5b883a829a14eb330b094d8b") {
+            id
+            user {
+                name
+                username
+            }
+            products {
+                product {
+                    id
+                    name
+                    image
+                    description
+                    seller {
+                        id
+                        shopName
+                        name
+                    }
+                }
+                itemCount
+                selectedSize
+            }
+            discount
+            total
+            shipping {
+                status
+                address {
+                    address
+                    street
+                    city
+                    state
+                    zipcode
+                }
+            }
+            date
+            status {
+                packed
+                shipped
+                confirmed
+                delivered
+            }
+            payment {
+                status
+                mode
+            }
+        }
+    }
+`;
+
+export const GET_ALL_PRODUCTS = gql`
+    query {
+        getProductBySeller(sellerID: "5b883a829a14eb330b094d8b") {
+            id
+            name
+            image
+            description
+            price
+            sizes
+            seller {
+                name
+                shopName
+            }
+        }
+    }
+`;
